c386e5ded9f4d1007b365124a6b152db4cb11d6a
mahdihemmat58-dev <mahdihemmat58@gmail.com>
2026-07-12 09:17:21 +0330

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{ padding: '20px', fontFamily: 'Arial' }}>
+      <h1>Apex Smart Marketing Workspace</h1>
+      <p>Backend: Connected</p>
+      <p>Dashboard in development...</p>
+      <button onClick={() => alert('Project Created!')}>Create New Project</button>
+    </div>
+  );
+}
+
+export default App;
